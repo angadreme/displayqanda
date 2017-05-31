a502809ba2cb1afb161f63ed57a3ceee99f4e7a3
Rosa Andrews <rosamaya.27@hotmail.com>
2017-05-31 15:47:17 -0700

    qComment not required

--- api/questions.ts
+++ api/questions.ts
@@ -7,14 +7,17 @@ router.get('/', (req, res) => {
   Question.find().then((questions) => res.json(questions));
 });
 
+router.get('/lesson/:id/questions', (req, res) => {
+  Question.find({lessonID: req.params.id})
+  .then((matches) => res.json(matches))
+  .catch((err) => res.json(err));
+});
+
 router.get('/:id', (req, res) => {
   Question.findById(req.params.id)
   .then((foundquestion) => res.json(foundquestion));
 });
 
-router.get('/lessons/:id/questions', (req, res) => {
-  Question.find({lessonID: req.params.id})
-  .then((matches) => res.json(matches));
-});
+
 
 export default router;
