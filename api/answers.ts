d9f69aca0d5a3a12ed22d6d6bbbddc9e23423d87
DESKTOP-O8MO24O\angad <angadreme@gmail.com>
2017-06-13 14:34:26 -0700

    current course lessons and trending lists

--- api/answers.ts
+++ api/answers.ts
@@ -4,20 +4,33 @@ import Answer from '../models/answers';
 let router = express.Router();
 
 router.get('/', (req, res) => {
+  console.log("get/");
   Answer.find().then((answers) => res.json(answers));
 });
 
 router.get('/:id', (req, res) => {
+  console.log('get/:id');
   Answer.findById(req.params.id)
   .then((foundanswer) => res.json(foundanswer));
 });
 
-router.get('/:date', (req, res) => {
-  Answer.find({aDate: {$where: "Answer.aDate > req.params.date"}})
-  .then((matches) => res.json(matches));
+router.get('/getDate/:date', (req, res) => {
+  console.log("here 1");
+  let rightNow = new Date().toJSON();
+  Answer.find({aDate: {
+        $lte : rightNow,
+        $gt : req.params.date
+  }})
+  .then((matches) => {
+    console.log("here 2")
+    res.json(matches)
+    console.log(matches)
+  })
+  .catch((err) => console.log(err));
 });
 
 router.get('/questions/:id/answers', (req, res) => {
+  console.log("questionsandanswers");
   Answer.find({questionId: req.params.id})
   .then((matches) => res.json(matches));
 });
